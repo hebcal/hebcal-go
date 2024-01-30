100ebb496e995573d810d06101c15732f06fb918
MaxBGreenberg <122072621+MaxBGreenberg@users.noreply.github.com>
2024-01-30 14:43:26 -0500

    Update zmanim_test.go
    added missing comma in bein hashmashos test unit

--- zmanim/zmanim_test.go
+++ zmanim/zmanim_test.go
@@ -107,7 +107,7 @@ func TestZmanimTelAviv(t *testing.T) {
 		zman.PlagHaMincha(),
 		zman.Sunset(),
 		zman.Dusk(),
-		zman.BeinHashmashos()
+		zman.BeinHashmashos(),
 		zman.Tzeit(8.5),
 	}
 	actual := make([]string, 18)
